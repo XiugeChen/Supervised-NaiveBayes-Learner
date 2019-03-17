acda1693d12dfb9cee0823164f86358b06b33aa4
SimonChen <cxgsimon@outlook.com>
2019-03-17 16:20:43 +1100

    Initialization

---
+++ NaiveBayesLeaner.py
@@ -0,0 +1,28 @@
+# Project1 for course Machine Learning COMP30027 at Unimelb
+# Coordinator: Tim Baldwin
+# Supervised Naive Bayes learner
+# Xiuge Chen
+# 2019.03.15
+
+
+def main(): 
+    return
+
+def preprocess():
+    return
+    
+def train():
+    return
+    
+def predict():
+    return
+    
+def evaluate():
+    return
+    
+def info_gain():
+    return
+    
+# make the main function work
+if __name__ == "__main__":
+    main()
