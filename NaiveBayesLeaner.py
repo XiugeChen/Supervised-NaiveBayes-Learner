6ed9add672b5242f194c3d642cfe88dc4076058e
Xiuge Chen <xiuge.chen@student.unimelb.edu.au>
2019-03-23 23:19:47 +1100

    NaiveBayesLearner_1.0

--- NaiveBayesLeaner.py
+++ NaiveBayesLeaner.py
@@ -6,6 +6,7 @@
 
 import os
 import sys
+import math
 
 ##### SYSTEM SETTINGS #####
 
@@ -14,26 +15,9 @@ FOLDER_PATH = "./2019S1-proj1-data/"
 # file name that contains header information
 HEADER_FILE = "headers.txt"
 # epsilon smoothing value
-EPSILON = 1
+EPSILON = sys.float_info.epsilon
 
 ##### END SYSTEM SETTINGS #####
-
-#### MAIN FUNCTION ####
-# main function that initialize and execute this program
-def main(): 
-    # get all file paths and header mapping for each file
-    fileNames = getFileNames(FOLDER_PATH)
-    
-    for fileName in fileNames:
-        dataSet = preprocess(fileName)
-        
-        model = train(dataSet)
-        
-        predict(model, dataSet[:10])
-    
-    return
-    
-#### END MAIN FUNCTION ####
     
 #### DATA PROCESSING ####
 # opens a data file, and converts it into a usable format dataset with proper header at its first row
@@ -82,6 +66,12 @@ def getFileNames(folderPath):
         filePaths.append(dicName)
     
     return filePaths
+
+# partition given data set into training and testing data
+# INPUT: dataset
+# OUTPUT: list of training dataset, list of testing dataset
+def partition_testOnTrain(dataSet):   
+    return dataSet.copy(), dataSet.copy()
     
 #### END DATA PROCESSING ####
 
@@ -94,80 +84,78 @@ def getFileNames(folderPath):
 # values, values are counts or probabilities) representing the conditional probabilities P(a|c);
 def train(dataSet):
     # class distribution and conditional probabilities
-    Pc, Pac = {}, {}  
-    classCount, dataHeader = 0, dataSet[0][0]
+    pc, pac = {}, {}  
+    dataHeader = dataSet[0][0]
 
     for instance in dataSet[1:]:
         # count priors and initialize posteriors
         classTag, attributes = instance[1], instance[0]
         
-        if classTag in Pc:
-            Pc[classTag] = Pc[classTag] + 1
-            classCount += 1
-                
+        if classTag in pc:
+            pc[classTag] += 1
         else:
-            Pc[classTag], Pac[classTag] = 1, {}
+            pc[classTag], pac[classTag] = 1, {}
         
-        # count posteriors, Pac represent conditional probabilities
+        # count posteriors, pac represent conditional probabilities
         for i in range(0, len(attributes)):
             attributeName, attributeValue = dataHeader[i], attributes[i]
         
-            if attributeName in Pac[classTag]:
-                if attributeValue in Pac[classTag][attributeName]:
-                    Pac[classTag][attributeName][attributeValue] = Pac[classTag][attributeName][attributeValue] + 1
+            if attributeName in pac[classTag]:
+                if attributeValue in pac[classTag][attributeName]:
+                    pac[classTag][attributeName][attributeValue] += 1
                 else:
-                    Pac[classTag][attributeName][attributeValue] = 1
+                    pac[classTag][attributeName][attributeValue] = 1
             else:
-                Pac[classTag][attributeName] = {}
-                Pac[classTag][attributeName][attributeValue] = 1
+                pac[classTag][attributeName] = {}
+                pac[classTag][attributeName][attributeValue] = 1
                 
     # normalise counts to get posteriors probabilities of each values of each attributes of each classes
     # eliminate missing value 
-    for singleClass in Pac.keys():
-        for attribute in Pac[singleClass].keys():
-            valueKeys = Pac[singleClass][attribute].keys()
-            numInstance = Pc[singleClass]
+    for singleClass in pac.keys():
+        for attribute in pac[singleClass].keys():
+            valueKeys = pac[singleClass][attribute].keys()
+            numInstance = pc[singleClass]
             
             # if there is missing value, not contribute to the counts/probability estimates
             if '?' in valueKeys:
-                numInstance = numInstance - Pac[singleClass][attribute]['?']
-                del Pac[singleClass][attribute]['?']
+                numInstance = numInstance - pac[singleClass][attribute]['?']
+                del pac[singleClass][attribute]['?']
             
             for valueKey in valueKeys:
-                Pac[singleClass][attribute][valueKey] = Pac[singleClass][attribute][valueKey] / numInstance
+                pac[singleClass][attribute][valueKey] = pac[singleClass][attribute][valueKey] / numInstance
     
     # normalise counts to get priors probabilities of each classes
-    for singleClass in Pc.keys():
-        Pc[singleClass] = Pc[singleClass] / classCount
+    for singleClass in pc.keys():
+        pc[singleClass] = pc[singleClass] / len(dataSet[1:])
     
-    return (Pc, Pac)
+    return (pc, pac)
     
 #### END TRAINING STAGE ####
 
 #### PREDICTING STAGE ####
 
-# predict classes distribution for the test dataset, for new values using epsilon smoothing method
+# predict classes distribution for the test dataset, for each probabilities use log-transformation to prevent underflow, for new values using epsilon smoothing method
 # INPUT: the Naive Bayes model, usable format dataSet with proper header at first row
 # OUTPUT: a list of class labels
 def predict(model, dataSet):
     dataHeader, predictClass = dataSet[0][0], []
-    Pc, Pac = model
+    pc, pac = model
 
     for instance in dataSet[1:]:
-        maxClass, maxProb = "", 0
+        maxClass, maxProb = "", float('-inf')
         attributes = instance[0]
         
-        for singleClass in Pac.keys():
+        for singleClass in pac.keys():
             # calculate probabilities for each class, replace 0 with epsilon
-            prob = Pc[singleClass]
+            prob = math.log(pc[singleClass])
             
             for i in range(0, len(attributes)):
                 attributeName, attributeValue = dataHeader[i], attributes[i]
                 
-                if attributeValue in Pac[singleClass][attributeName].keys():
-                    prob *= Pac[singleClass][attributeName][attributeValue]
+                if attributeValue in pac[singleClass][attributeName].keys():
+                    prob += math.log(pac[singleClass][attributeName][attributeValue])
                 else:
-                    prob *= EPSILON
+                    prob += math.log(EPSILON)
             
             if prob > maxProb:
                 maxClass, maxProb = singleClass, prob
@@ -178,14 +166,137 @@ def predict(model, dataSet):
     
 #### END PREDICTING STAGE ####
 
+#### EVALUATION STAGE ####
+
+# output evaluation metric(s) for single model and test data, or sufficient information so that they can be easily calculated by hand
+# INPUT: trained model, usable dataset with header for testing
 # OUTPUT: void, but prints the necessary evaluation metric information
-def evaluate():
+# print example:
+#   A B
+# A 2 1
+# B 0 3
+# meaning: 2 instances of class A are correctly identified as A, 1 is mistakenly identified as B, all 3 instances of B are correctly identified as B, none of them is mistakenly identified as A
+def evaluate(model, testSet):
+    # get correct label array
+    correctLabels = []
+    for instance in testSet[1:]:
+        correctLabels.append(instance[1])
+    
+    resultLabels = predict(model, testSet)
+    
+    # get result 2d map, primary key is each primary class, secondary key and value are the number of other class primary class has been identified into
+    # ex. {'A': {'A': 2, 'B': 1}} means for total 3 test instances of A, 2 of them are correctly identified as A itself, 1 is mistakenly identified as B
+    metricMap = {}
+    for i in range(0, len(correctLabels)):
+        correctLabel, resultLabel = correctLabels[i], resultLabels[i]
+        
+        if correctLabel in metricMap.keys():
+            if resultLabel in metricMap[correctLabel].keys():
+                metricMap[correctLabel][resultLabel] += 1
+            else:
+                metricMap[correctLabel][resultLabel] = 1
+        else: 
+            metricMap[correctLabel] = {}
+            metricMap[correctLabel][resultLabel] = 1
+            
+    # transform the result map to output matrix format and output results       
+    print("Class Idendification Matrix:")
+    print("\t" + "".join(key + "\t" for key in metricMap.keys()))
+    
+    for priKey in metricMap.keys():
+        countList = [priKey]
+        
+        for secKey in metricMap.keys():
+            if secKey in metricMap[priKey].keys():
+                countList.append(metricMap[priKey][secKey])
+            else:
+                countList.append(0)
+                
+        print("".join(str(count) + "\t" for count in countList))
+        
     return
 
-# OUTPUT: a list of values of Information Gain (one per attribute)
-def info_gain():
+# calculate the Information Gain (IG) for one (or each) attribute, relative to the class distribution
+# INPUT: A trained model
+# OUTPUT: a dictionary maps each attribute to its values of Information Gain
+def info_gain(model):
+    pc, pac = model
+    ig, attributes_values, output = 0, {}, {}
+    
+    # get entire attributes and value mapping list
+    attributes = []
+    for eachclass in pac:
+        attributes.extend(pac[eachclass].keys())  
+    # get unique attributes set
+    attributes = list(set(attributes))
+    
+    for attribute in attributes:
+        values = []
+        
+        for eachclass in pac:
+            values.extend(pac[eachclass][attribute].keys())
+        
+        values = list(set(values))
+        attributes_values[attribute] = values
+
+    # entropy for root, the entropy before splitting the tree using the attribute’s values
+    for eachclass in pc.keys():
+        ig -= pc[eachclass] * math.log(pc[eachclass], 2)
+    
+    for attribute in attributes_values.keys():
+        meanInfo = 0
+        # the weighted average of the entropy over the children after the split (Mean Information)
+        # Mean Information (attribute a) = sum_v( P(value v) * H(value v) )
+        for value in attributes_values[attribute]:
+            # P(a=v), prob_av
+            prob_av = 0
+            for eachclass in pac.keys():
+                if attribute in pac[eachclass].keys() and value in pac[eachclass][attribute].keys():
+                    prob_av += pac[eachclass][attribute][value] * pc[eachclass]
+        
+            # H(a=v) =  - sum_c( P(class c | a=v) * log(P(c | a=v)) ), h_av
+            # P(c | a=v) = (P(a=v | c) * P(c)) / P(a=v), prob_c_av
+            h_av = 0
+            for eachclass in pac.keys():
+                if attribute in pac[eachclass].keys() and value in pac[eachclass][attribute].keys():
+                    prob_c_av = (pac[eachclass][attribute][value] * pc[eachclass]) / prob_av
+                    h_av -= prob_c_av * math.log(prob_c_av, 2)          
+                
+            meanInfo += prob_av * h_av
+            
+        output[attribute] = ig - meanInfo
+
+    return output
+    
+#### END EVALUATION STAGE ####
+
+#### MAIN FUNCTION ####
+# main function that initialize and execute this program
+def main(): 
+    # get all file paths and header mapping for each file
+    fileNames = getFileNames(FOLDER_PATH)
+    
+    for fileName in fileNames:
+        print("File: " + fileName)
+    
+        dataSet = preprocess(fileName)
+        
+        trainSet, testSet = partition_testOnTrain(dataSet)
+        
+        model = train(trainSet)
+        
+        result = predict(model, testSet)
+        
+        evaluate(model, testSet)
+        
+        info_gain(model)
+        
+        print(info_gain)
+    
     return
     
 # make the main function work
 if __name__ == "__main__":
     main()
+    
+#### END MAIN FUNCTION ####
